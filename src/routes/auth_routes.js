cea6c9b414bcc4758c12ad77225b96feddc3075d
raminicano <raminicayes@gmail.com>
2024-07-18 10:38:05 +0000

    Feat: 회원가입, 로그인, 로그아웃, 리프레시토큰 처리

--- src/routes/auth_routes.js
+++ src/routes/auth_routes.js
@@ -4,7 +4,9 @@ const authController = require('../controllers/auth_controller');
 const authenticateToken = require('../middlewares/auth_middleware');
 
 
-router.post('/register', authController.register);
+router.post('/signup', authController.signup);
 router.post('/login', authController.login);
+router.post('/refresh', authController.refresh);
+router.post('/logout', authController.logout);
 
 module.exports = router;
